3cd11d665410da9d4a548ec5d6fdf8a90adc22ab
Shafi <patanshafiqa@gmail.com>
2024-05-07 19:33:33 +0530

    first commit

---
+++ src/test/java/hooks/Shafi.java
@@ -0,0 +1,13 @@
+package hooks;
+
+public class Shafi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+
+	}
+
+}
